336a150cbaf154f1374806d0a807cea55cd2e3f2
IvanVitomirovich <99694941+IvanVitomirovich@users.noreply.github.com>
2022-02-24 01:14:39 +0300

    Delete task_1.py

--- task_1.py
+++
@@ -1,9 +0,0 @@
-x = 1
-y = 2
-print(f'Переменная x - {x} \nПеременная y - {y}')
-string1 = input('Введите первую строку: ')
-number1 = int(input('Введите первое число: '))
-string2 = input('Введите вторую строку: ')
-number2 = int(input('Введите второе число: '))
-print(f'Введеные значения - {string1}, {number1}, {string2}, {number2}')
-
