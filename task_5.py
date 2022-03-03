db7c8c6d5752d32cf66e1a7ece7e091610a29800
IvanVitomirovichIvanVitomirovich <idyurkovich@mail.ru>
2022-03-03 17:47:37 +0300

    lesson5

---
+++ task_5.py
@@ -0,0 +1,13 @@
+import random
+
+with open('text6.txt', 'w') as new_file:
+    for el in range(10):
+        new_file.write(f'{random.randint(0, 10)} ')
+
+with open('text6.txt') as new_file:
+    numb_str = new_file.read().split()
+    sum = 0
+    for numb in numb_str:
+        sum += int(numb)
+
+print(sum)
